e579a4b86dcf6736ec5826a37a9336511c16a700
ChaeDoll <dlacodbs0627@gmail.com>
2024-04-08 18:14:18 +0900

    240408 chae

--- screens/(main)/ChatRoom.js
+++ screens/(main)/ChatRoom.js
@@ -1,10 +1,32 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { ScrollView, View } from "react-native";
+import useComment from "../../hook/usecomment";
+import { useEffect, useState } from "react";
 
-export default function ChatRoom() {
+export default function ChatRoom({navigation, route}) {
+    const {getComments, postComment, deleteComment} = useComment();
+    const [commentList, setCommentList] = useState();
+    const emotion = "HAPPY";
+    // comment, createDate, emotion, id, user:{createDate, id, nickname}
+    useEffect(()=>{
+        getComments(emotion).then(res=>()=>{
+            setCommentList(res);
+        });
+    },[])
     return (
         <LinearGradient colors={['#9CB7FF', '#DAD1FF']} end={{x:0.5, y:0.6}} style={{height:'100%'}}>
-            <ScrollView></ScrollView>
+            <ScrollView style={{}}>
+                {commentList?.map((comment, index)=>{
+                    console.log(comment);
+                    return (
+                        <View key={index} style={{backgroundColor:'blue'}}>
+                            <Text>{comment.user.nickname}</Text>
+                            <Text>{comment.comment}</Text>
+                            <Text>{comment.createDate}</Text>
+                        </View>
+                    )
+                })}
+            </ScrollView>
         </LinearGradient>
     )
 }
