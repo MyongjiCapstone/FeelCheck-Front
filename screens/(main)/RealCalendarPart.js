83f29a9436741b4794e55815dd396cff0083970e
Jinjeongjun <wlswjdwns1@naver.com>
2024-05-07 00:02:54 +0900

    0506 jun can put emozi

--- screens/(main)/RealCalendarPart.js
+++ screens/(main)/RealCalendarPart.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-class RealCalendarPart extends React.Component {
-  months = [
+export default function RealCalendarPart() {
+  const months = [
     '1월',
     '2월',
     '3월',
@@ -21,43 +21,48 @@ class RealCalendarPart extends React.Component {
     '12월',
   ];
 
-  _onPress = (item) => {
-    this.setState(() => {
-      if (!item.match && item != -1) {
-        this.state.activeDate.setDate(item);
-        return this.state;
-      }
+  const weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+
+  const nDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  const [activeDate, setActiveDate] = useState(new Date());
+
+  const [emotions, setEmotions] = useState(['😊', '😭']);
+  console.log(emotions);
+
+  const _onPress = (item) => {
+    if (!item.match && item !== -1) {
+      setActiveDate((prevDate) => {
+        const newDate = new Date(prevDate);
+        newDate.setDate(item);
+        return newDate;
+      });
+    }
+  };
+
+  const changeMonth = (n) => {
+    setActiveDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() + n);
+      return newDate;
     });
   };
 
-  changeMonth = (n) => {
-    this.setState(() => {
-      this.state.activeDate.setMonth(this.state.activeDate.getMonth() + n);
-      return this.state;
-    });
-  };
-
-  weekDays = ['일', '월', '화', '수', '목', '금', '토'];
-
-  nDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  state = {
-    activeDate: new Date(),
-  };
-
-  generateMatrix() {
+  const generateMatrix = () => {
     var matrix = [];
     // Create header
-    matrix[0] = this.weekDays;
+    matrix[0] = weekDays;
 
-    var year = this.state.activeDate.getFullYear();
-    var month = this.state.activeDate.getMonth();
+    // console.log(weekDays);
+
+    var year = activeDate.getFullYear();
+    var month = activeDate.getMonth();
     var firstDay = new Date(year, month, 1).getDay();
 
-    var maxDays = this.nDays[month];
-    if (month == 1) {
+    var maxDays = nDays[month];
+    if (month === 1) {
       // 2월이 윤년인 경우
-      if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+      if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
         maxDays += 1;
       }
     }
@@ -67,7 +72,7 @@ class RealCalendarPart extends React.Component {
       matrix[row] = [];
       for (var col = 0; col < 7; col++) {
         matrix[row][col] = -1;
-        if (row == 1 && col >= firstDay) {
+        if (row === 1 && col >= firstDay) {
           // Fill in rows only after the first day of the month
           matrix[row][col] = counter++;
         } else if (row > 1 && counter <= maxDays) {
@@ -79,121 +84,139 @@ class RealCalendarPart extends React.Component {
     }
 
     return matrix;
-  }
+  };
 
-  render() {
-    var matrix = this.generateMatrix();
+  const matrix = generateMatrix();
 
-    var rows = [];
-    rows = matrix.map((row, rowIndex) => {
-      var rowItems = row.map((item, colIndex) => {
-        return (
-          <View
-            style={{
-              flex: 1,
-              height: 50,
-              borderWidth: rowIndex > 5 && item == -1 ? 0 : 0.5, // 테두리
-              borderColor: '#E6E6E6', // 테두리 색상
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 16,
-                flex: 1,
-                textAlign: 'center',
-                paddingTop: rowIndex == 0 ? 15 : 0,
-                // Highlight header
-                backgroundColor: rowIndex == 0 ? '#F4F4F4' : '#fff',
-                // 일요일은 빨간색, 토요일은 파란색으로
-                color: colIndex == 6 ? '#00a' : colIndex == 0 ? '#a00' : '#000',
-                // 선택한 날짜 볼드처리
-                fontWeight:
-                  item == this.state.activeDate.getDate() ? 'bold' : '',
-              }}
-              onPress={() => this._onPress(item)}
-            >
-              {item != -1 ? item : ''}
-            </Text>
-          </View>
-        );
-      });
-
+  const rows = matrix.map((row, rowIndex) => {
+    const rowItems = row.map((item, colIndex) => {
+      const dateKey = `${activeDate.getFullYear()}-${
+        activeDate.getMonth() + 1
+      }-${item}`;
+      const emotion = emotions[dateKey];
+      // console.log(item);
       return (
         <View
           style={{
             flex: 1,
-            flexDirection: 'row',
-            padding: 25,
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            height: 50,
+            borderWidth: rowIndex > 5 && item === -1 ? 0 : 0.5, // 테두리
+            borderColor: '#E6E6E6', // 테두리 색상
           }}
+          key={`${rowIndex}-${colIndex}`}
         >
-          {rowItems}
+          <Text
+            style={{
+              fontSize: 16,
+              flex: 1,
+              textAlign: 'center',
+              paddingTop: rowIndex === 0 ? 15 : 0,
+              // Highlight header
+              backgroundColor: rowIndex === 0 ? '#F4F4F4' : '#fff',
+              // 일요일은 빨간색, 토요일은 파란색으로
+              color: colIndex === 6 ? '#00a' : colIndex === 0 ? '#a00' : '#000',
+              // 선택한 날짜 볼드처리
+              fontWeight: item === activeDate.getDate() ? 'bold' : '',
+            }}
+            onPress={() => _onPress(item)}
+          >
+            {item !== -1 ||
+            item == '월' ||
+            item == '화' ||
+            item == '수' ||
+            item == '목' ||
+            item == '금' ||
+            item == '토' ||
+            item == '일' ? (
+              <>
+                <Text>{item}</Text>
+                <Text>{'\n'}</Text>
+                {/* {emotion && <Text>{emotion}</Text>} */}
+                <Text>{emotions[0]}</Text>
+                {/* 일단 0번 인덱스에 해당하는 웃음 이모지로 다 넣어봄 */}
+              </>
+            ) : (
+              ''
+            )}
+          </Text>
         </View>
       );
     });
 
     return (
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View
-          style={{
-            borderRadius: 10,
-            height: hp('8%'),
-            flexDirection: 'row',
-            backgroundColor: '#E6E6E6',
-          }}
-        >
-          <TouchableOpacity onPress={() => this.changeMonth(-1)}>
-            <Text
-              style={{
-                marginTop: hp('1.5%'),
-                fontSize: 30,
-                textAlign: 'left',
-                marginLeft: 20,
-              }}
-            >
-              {'<'}
-            </Text>
-          </TouchableOpacity>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          padding: 25,
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+        key={rowIndex}
+      >
+        {rowItems}
+      </View>
+    );
+  });
+
+  return (
+    <View style={{ flex: 1, flexDirection: 'column' }}>
+      <View
+        style={{
+          borderRadius: 10,
+          height: hp('8%'),
+          flexDirection: 'row',
+          backgroundColor: '#E6E6E6',
+        }}
+      >
+        <TouchableOpacity onPress={() => changeMonth(-1)}>
           <Text
             style={{
               marginTop: hp('1.5%'),
-              fontWeight: '600',
               fontSize: 30,
-              textAlign: 'center',
-              marginLeft: 10,
+              textAlign: 'left',
+              marginLeft: 20,
             }}
           >
-            {this.months[this.state.activeDate.getMonth()]} &nbsp;
+            {'<'}
           </Text>
-          <TouchableOpacity onPress={() => this.changeMonth(+1)}>
-            <Text
-              style={{
-                marginTop: hp('1.5%'),
-                fontSize: 30,
-                textAlign: 'left',
-              }}
-            >
-              {'>'}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View
+        </TouchableOpacity>
+        <Text
           style={{
-            backgroundColor: 'white',
-            flex: 1,
-            flexDirection: 'row',
-            // marginHorizontal: wp('-14.5%'), // 최후의 수단;;
-            borderRadius: 10,
+            marginTop: hp('1.5%'),
+            fontWeight: '600',
+            fontSize: 30,
+            textAlign: 'center',
+            marginLeft: 10,
           }}
         >
-          <View style={{ flex: 1 }} />
-          <View style={{ flex: 10 }}>{rows}</View>
-          <View style={{ flex: 1 }} />
-        </View>
+          {months[activeDate.getMonth()]} &nbsp;
+        </Text>
+        <TouchableOpacity onPress={() => changeMonth(+1)}>
+          <Text
+            style={{
+              marginTop: hp('1.5%'),
+              fontSize: 30,
+              textAlign: 'left',
+            }}
+          >
+            {'>'}
+          </Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
+      <View
+        style={{
+          backgroundColor: 'white',
+          flex: 1,
+          flexDirection: 'row',
+          //marginHorizontal: wp('-14.5'), // 최후의 수단;;
+          borderRadius: 10,
+        }}
+      >
+        <View style={{ flex: 1 }} />
+        <View style={{ flex: 10 }}>{rows}</View>
+        <View style={{ flex: 1 }} />
+      </View>
+    </View>
+  );
 }
-
-export default RealCalendarPart;
